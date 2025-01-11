5f5b1ab9ced997f6f6c1d0a9906d7ddf3f984a9b
Rohit Tomar <heartstealarrohitthakur@gmail.com>
2025-01-12 00:24:58 +0530

    text color change green when counte++ and red when -- with the use of useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { increment,asyncdecerement } from './store/actions/counterActions'
 
@@ -9,18 +9,26 @@ const App = () => {
   const {counter} = useSelector((state)=>state);
   console.log(counter);
 
+  const h1Ref = useRef(null)
+
   const IncreaseCount=()=>{
+    h1Ref.current.style.color='green';
     dispatch(increment())
   }
 
   const DecreaseCount=()=>{
+    h1Ref.current.style.color='red';
+
+
     dispatch(asyncdecerement())
   }
 
     return (
     <div className='bg-gray-400 h-screen w-full flex items-center justify-center text-center'>
       <div className='w-1/3 h-1/3flex flex-col justify-between'>
-      <h1 className='text-red-500 text-7xl mb-14'>{counter.value}</h1>
+      <h1
+      ref={h1Ref} 
+      className='text-red-500 text-7xl mb-14'>{counter.value}</h1>
       <div className='flex justify-between px-10'>
         <button 
         onClick={()=>{
